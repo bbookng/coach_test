8ea341865785bc4d9c7947a4407dedf8e97c1632
bbookng <bbookng@gmail.com>
2023-07-30 23:13:29 +0900

    test

---
+++ coach/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={"/img/jjanggu.png"} alt="logo" />
+        <p>
+          안녕하세요 부울경 2반 교육생 여러분 !
+        </p>
+        <p> -부울경 실습코치 김보경- </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
